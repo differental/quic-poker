5b2e521c38a01fd16641a05ce1afea6a5292ae3f
Brian Chen <enderchen004@outlook.com>
2026-06-15 17:40:44 +0100

    feat: get the certs working

--- net/src/lib.rs
+++ net/src/lib.rs
@@ -1,14 +1,41 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+use std::{error::Error, sync::Arc};
+
+use quinn::{
+    ClientConfig, ServerConfig,
+    crypto::rustls::{QuicClientConfig, QuicServerConfig},
+};
+use rustls::pki_types::{CertificateDer, PrivateKeyDer, PrivatePkcs8KeyDer};
+
+pub fn configure_server(
+    cert: CertificateDer<'static>,
+    key: PrivatePkcs8KeyDer<'static>,
+) -> Result<ServerConfig, Box<dyn Error>> {
+    let crypto = rustls::ServerConfig::builder_with_provider(Arc::new(
+        rustls::crypto::ring::default_provider(),
+    ))
+    .with_safe_default_protocol_versions()?
+    .with_no_client_auth()
+    .with_single_cert(vec![cert], PrivateKeyDer::from(key))?;
+
+    Ok(ServerConfig::with_crypto(Arc::new(
+        QuicServerConfig::try_from(crypto)?,
+    )))
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+pub fn configure_client() -> Result<ClientConfig, Box<dyn Error>> {
+    // Use the Mozilla CA root certificate store
+    let roots = rustls::RootCertStore {
+        roots: webpki_roots::TLS_SERVER_ROOTS.to_vec(),
+    };
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+    let crypto = rustls::ClientConfig::builder_with_provider(Arc::new(
+        rustls::crypto::ring::default_provider(),
+    ))
+    .with_safe_default_protocol_versions()?
+    .with_root_certificates(roots)
+    .with_no_client_auth();
+
+    Ok(ClientConfig::new(Arc::new(QuicClientConfig::try_from(
+        crypto,
+    )?)))
 }
